af7eac6332e0e19f99876077c9083a3aab5802fe
den-bulaev <denisbulaev7@gmail.com>
2022-08-07 16:48:09 +0300

    tests were finished

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,10 +1,50 @@
 'use strict';
 
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+  const { restoreNames } = require('./restoreNames');
 
-  it('should ', () => {
+  it(`should add 'firstName' field to user`, () => {
+    const users = [{ fullName: 'Boba Fett' }, { fullName: 'Obi-wan Kenobi' }];
 
+    restoreNames(users);
+
+    expect(users).toEqual([{
+      fullName: 'Boba Fett', firstName: 'Boba',
+    }, {
+      fullName: 'Obi-wan Kenobi', firstName: 'Obi-wan',
+    }]);
+  });
+
+  it(`should work with inner extra spaces`, () => {
+    const users = [{ fullName: 'Boba   Fett' }];
+
+    restoreNames(users);
+
+    expect(users).toEqual([{
+      fullName: 'Boba   Fett', firstName: 'Boba',
+    }]);
+  });
+
+  it(`should work with outer extra spaces`, () => {
+    const users = [{ fullName: '  Boba Fett  ' }];
+
+    restoreNames(users);
+
+    expect(users).toEqual([{
+      fullName: '  Boba Fett  ', firstName: 'Boba',
+    }]);
+  });
+
+  it(`should not overwrite 'firstName' field`, () => {
+    const users = [{
+      fullName: 'Bob Fett', firstName: 'Boba',
+    }];
+
+    restoreNames(users);
+
+    expect(users).toEqual([{
+      fullName: 'Bob Fett', firstName: 'Boba',
+    }]);
   });
 
   // write tests here
